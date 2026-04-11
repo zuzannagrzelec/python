6e377b166b745ce03b383fb9717dcf44e056dbc8
Zuzanna Grzelec <zuzanna.grzelec@gmail.com>
2026-04-11 08:16:45 +0200

    Add lupy.py with various list manipulations, including element counting and conditional checks

---
+++ lupy.py
@@ -0,0 +1,55 @@
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# numbers2=[]
+
+# numbers.reverse()
+# # numbers[2] = 100
+# for number in numbers:
+#     numbers2.append(number * 2)
+#     print(numbers2)
+# print(numbers2)
+# print(numbers)
+# numbers.append(11)
+# print(numbers)
+
+# sum = sum(numbers)
+# # print(sum)
+# list=["Przemek", "Kasia", "Zosia", "Adam", "Ewa"]
+# list_of_name_characters=[]
+
+# for name in list:
+#     list_of_name_characters.append(len(name))
+
+# print(list_of_name_characters)
+# print(list)
+# fruits=["apple", "banana", "cherry", "orange", "strawberry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# list=[1,2,3,4,5]
+# for number in list:
+#     if number % 2 == 0:
+# #         print(number)
+# names=["ala", "olek",'basia']
+# for name in names:
+# #     print("DEbil " + name)
+# list=[4,10,15,20]
+# for number in list:
+#     if number > 10:
+#         print("DUŻA")
+#     elif number == 10:
+#         print("równa sie 10")
+#     else:
+# #         print("MAŁA lub równa 10")
+# list=[1,2,3,4,5,6]
+# list2=[]
+# for number in list:
+#     if number % 2 == 0:
+#         list2.append(number)
+
+# print(list2)
+list=[3,4,2,5,1,4]
+suma=0
+for number in list:
+    if number >=3:
+        suma= suma + 1
+print(suma)
